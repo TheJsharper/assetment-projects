6ea94a40f5a767fa426370713ee05f3f6b740fe3
Pedro Salomon <pedro.salomon@gmail.com>
2025-06-20 13:43:49 +0200

    fixed tested app

--- ist-coding-challenge/source/routers/DummyRouter.ts
+++ ist-coding-challenge/source/routers/DummyRouter.ts
@@ -1,8 +1,7 @@
 import { Router } from "express";
-import { z } from "zod";
 import DummyController from "../controllers/DummyController.js"; // TO_CHANGE: naming
-import { Configuration } from "../models/ConfigurationModel.js";
 import validVatValidator from "../middlewares/validate-post-request.middleware.js";
+import { Configuration } from "../models/ConfigurationModel.js";
 
 let dummyController: DummyController; // TO_CHANGE: naming
 
---
+++ ist-coding-challenge/tests/app.test.ts
@@ -0,0 +1,9 @@
+
+describe("simple test", ()=>{
+
+
+    it('testing', ()=>{
+        expect(1+1).toBe(2)
+    })
+
+})
