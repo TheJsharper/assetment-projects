724975bfdea29b8d73cf4ed5764448b80825d455
Pedro Salomon <pedro.salomon@gmail.com>
2025-06-23 22:55:17 +0200

    e2e server testing

---
+++ ist-coding-challenge-nest/test/server.spec.ts
@@ -0,0 +1,103 @@
+
+import { INestApplication } from "@nestjs/common";
+import { Test, TestingModule } from "@nestjs/testing";
+import { AppModule } from "src/app.module";
+import request from 'supertest';
+import { App } from "supertest/types";
+
+describe('Server test suite', () => {
+
+    let app: INestApplication<App>;
+
+    beforeEach(async () => {
+        const moduleFixture: TestingModule = await Test.createTestingModule({
+            imports: [AppModule],
+        }).compile();
+
+        app = moduleFixture.createNestApplication();
+        await app.init();
+    })
+    afterEach(() => {
+        app.close();
+    })
+
+
+    it('simple server post handler /valid-vat/ testing valid payload', async () => {
+
+        const paylaod = {
+            body: {
+                countryCode: "DE",
+                vat: "12345678"
+            }
+        };
+        const res = await request(app.getHttpServer()).post(`/valid-vat/`)
+            .send(paylaod)
+            .set('Accept', 'application/json')
+            .set('Content-Type', 'application/json')
+            .expect("Content-Type", /json/)
+            .expect(200);
+
+    }, 10000)
+    it('simple server post handler /valid-vat/ testing invalid countryCode length payload', async () => {
+
+        const paylaod = {
+            body: {
+                countryCode: "DEX",
+                vat: "123456789"
+            }
+        };
+        const res = await request(app.getHttpServer(),).post(`/valid-vat/`)
+            .send(paylaod)
+            .set('Accept', 'application/json')
+            .set('Content-Type', 'application/json')
+            .expect("Content-Type", /json/)
+            .expect(400)
+        expect(res.body).toMatchObject<{ error: string, details: Array<{ message: string }> }>({
+            error: 'Invalid data',
+            details: [{ message: 'contryCode must be 2 characters length' }]
+        });
+
+    })
+    it('simple server post handler /valid-vat/ testing invalid vat length payload', async () => {
+
+        const paylaod = {
+            body: {
+                countryCode: "DE",
+                vat: "12345678910112"
+            }
+        };
+        const res = await request(app.getHttpServer()).post(`/valid-vat/`)
+            .send(paylaod)
+            .set('Accept', 'application/json')
+            .set('Content-Type', 'application/json')
+            .expect("Content-Type", /json/)
+            .expect(400)
+        expect(res.body).toMatchObject<{ error: string, details: Array<{ message: string }> }>({
+            error: 'Invalid data',
+            details: [{ message: 'vat must be between 8 to 12 length' }]
+        });
+
+    })
+    it('simple server post handler /valid-vat/ testing invalid vat length payload and countryCode length payload', async () => {
+
+        const paylaod = {
+            body: {
+                countryCode: "DEX",
+                vat: "12345678910112"
+            }
+        };
+        const res = await request(app.getHttpServer()).post(`/valid-vat/`)
+            .send(paylaod)
+            .set('Accept', 'application/json')
+            .set('Content-Type', 'application/json')
+            .expect("Content-Type", /json/)
+            .expect(400)
+        expect(res.body).toMatchObject<{ error: string, details: Array<{ message: string }> }>({
+            error: 'Invalid data',
+            details: [{ message: 'contryCode must be 2 characters length' }, { message: 'vat must be between 8 to 12 length' }]
+        });
+
+    })
+
+
+})
