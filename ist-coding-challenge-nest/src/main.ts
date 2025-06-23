360fbb52f620f376506dd1a5b859a109036c911c
Pedro Salomon <pedro.salomon@gmail.com>
2025-06-23 22:14:17 +0200

    include json files building

--- ist-coding-challenge-nest/src/main.ts
+++ ist-coding-challenge-nest/src/main.ts
@@ -4,6 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
     const config = new DocumentBuilder()
     .setTitle('Demo rest server')
     .setDescription('A simple server')
